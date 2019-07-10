19d125809caec4f1f274b97d0ea179ef6641fbaf
Adam Argyle <atom@argyleink.com>
2019-07-10 15:49:32 -0700

    slides started

--- app/index.js
+++ app/index.js
@@ -1,9 +1,8 @@
-  // import 'https://slyd.netlify.com/bundle.js'
+import 'https://slyd.netlify.com/bundle.js'
+
+if (window.location.hash)
+  window.onload = () =>
+    window.location = window.location
 
  // if (navigator.serviceWorker && !navigator.serviceWorker.controller)
  //   navigator.serviceWorker.register('./service-worker.js')
-
-// navigator.serviceWorker.getRegistrations().then(function(registrations) {
-//  for(let registration of registrations) {
-//   registration.unregister()
-// } })
